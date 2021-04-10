d843f5a3d7ea741cd5647a9fa236d4f19a8c621d
drivemovie310597 <drivemovie310597@gmail.com>
2021-04-10 18:06:33 +0700

    V2

--- expense_tracker/tests.py
+++ expense_tracker/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+def two_integer(a, b):
+    return a + b  # TDD Practice
+
+
+class Test(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_integer(1, 2), 3)  # 3 is the expected result
